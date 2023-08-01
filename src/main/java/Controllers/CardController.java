73ae4a842e8ac5649c22cb6eca9de5b006ab787b
fahima <120641842+fahima-alhabsi@users.noreply.github.com>
2023-08-01 11:54:46 +0400

    add proper annotations

--- src/main/java/Controllers/CardController.java
+++ src/main/java/Controllers/CardController.java
@@ -1,9 +1,14 @@
 package Controllers;
 
+import Reposetries.CardRepositry;
+import Services.CardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+
 public class CardController {
+    @Autowired
+    CardService cardService ;
 }
--- src/main/java/Models/BoardModel.java
+++ src/main/java/Models/BoardModel.java
@@ -1,13 +1,19 @@
 package Models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
 
 @Getter
 @Setter
 @Entity
 @Data
 public class BoardModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
 }
