cced22154990d02508e4d03cba3c998da612ddad
fahima <120641842+fahima-alhabsi@users.noreply.github.com>
2023-08-01 17:41:52 +0400

    add functions to the controller class

--- src/main/java/com/example/BOARD/API/Controllers/BoardController.java
+++ src/main/java/com/example/BOARD/API/Controllers/BoardController.java
@@ -1,15 +1,40 @@
 package com.example.BOARD.API.Controllers;
 
 import com.example.BOARD.API.Models.BoardModel;
+import com.example.BOARD.API.RequistObject.GetBoardRequistObject;
 import com.example.BOARD.API.Services.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 @RestController
 
 public class BoardController {
     @Autowired
     BoardService boardService;
+
+    @GetMapping("get")
+    public ResponseEntity<?> findAll() {
+
+        return ResponseEntity.ok(boardService.GetAllBoards());
+    }
+
+public void CreateBoard(GetBoardRequistObject getBoardRequistObject){
+BoardModel Board = new BoardModel();
+    Board.setName("NAME");
+    Board.setCreatedDate(new Date());
+Board.setIsActive(true);
+    boardService.CreateBoard(Board);
+
+}
+
+
+
+
 
 }
