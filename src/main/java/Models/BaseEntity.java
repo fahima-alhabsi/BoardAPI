279b8ee9e75e5e3c372ab66c5af191bc164137af
fahima <120641842+fahima-alhabsi@users.noreply.github.com>
2023-08-01 11:41:50 +0400

    create proper classes

---
+++ src/main/java/Models/BaseEntity.java
@@ -0,0 +1,9 @@
+package Models;
+
+import java.util.Date;
+
+public class BaseEntity {
+    Date createdDate;
+    Date updatedDate;
+    Boolean isActive;
+}
--- src/main/java/Services/BoardService.java
+++ src/main/java/Services/BoardService.java
@@ -1,7 +1,11 @@
 package Services;
 
+import Reposetries.BoardRepositry;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BoardService {
+    @Autowired
+    BoardRepositry boardRepositry;
 }
