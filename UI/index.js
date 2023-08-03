7a1c22c38f356afbbf1af365cc9d1b6519f78dd9
fahima <120641842+fahima-alhabsi@users.noreply.github.com>
2023-08-03 23:51:56 +0400

    editing javascript functions for Board

--- UI/index.js
+++ UI/index.js
@@ -1,268 +1,92 @@
+function createBoard() {
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
 
+  var raw = JSON.stringify({
+    "name": document.getElementById("boardName").value
+  });
 
-// //////////////////////////////////to create Board////////////////////////////////////////
+  var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+  };
 
-function createCard() {
-    var myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-    
-    var raw = JSON.stringify({
-        "createdDate": document.getElementById("createdDate").value,
-        "updatedDate": document.getElementById("updatedDate").value,
-        "isActive": document.getElementById("isActive").value,
-        "title": document.getElementById("title").value,
-        "section": document.getElementById("section").value,
-        "description": document.getElementById("description").value,
-    });
-    
-    var requestOptions = {
-        method: 'POST',
-        headers: myHeaders,
-        body: raw,
-        redirect: 'follow'
-    };
-    
-    fetch("http://localhost:8080/create", requestOptions)
-        .then(response => response.json())
-        .then(result => console.log(result))
-        .catch(error => console.log('error', error));
+  fetch("/create", requestOptions)
+    .then(response => {
+      if (response.status === 200) {
+        console.log("Board created successfully!");
+      } else {
+        console.log("Failed to create board.");
+      }
+    })
+    .catch(error => console.log('error', error));
 }
 
-//////////////////////////////////////////////////// to get all boards////////////////////////////
 
 function getAllBoards() {
-    var myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
+  var requestOptions = {
+    method: 'GET',
+    headers: {
+      "Content-Type": "application/json"
+    },
+    redirect: 'follow'
+  };
 
-    var requestOptions = {
-        method: 'GET',
-        headers: myHeaders,
-        redirect: 'follow'
-    };
+  fetch("/get", requestOptions)
+    .then(response => gitresponse.json())
+    .then((result) => {
+      result.forEach(board => {
+        let div = document.getElementById("boardsColumn");
+        div.innerHTML += "<h3 style='color: red'>Board ID: " + board.id + "</h3>";
+        div.innerHTML += "Board Name: " + board.name + "<br />";
+      });
+    })
+    .catch(error => console.log('error', error));
+}
 
-    fetch("http://localhost:8080/get", requestOptions)
-        .then(response => response.text())
-        .then(result => console.log(result))
-        .catch(error => console.log('error', error));
+function getBoardById(boardId) {
+  var requestOptions = {
+    method: 'GET',
+    headers: {
+      "Content-Type": "application/json"
+    },
+    redirect: 'follow'
+  };
+
+  fetch("/api/board/" + boardId, requestOptions)
+    .then(response => response.json())
+    .then((result) => {
+      let div = document.getElementById("boardByIdColumn");
+      div.innerHTML = "<h3 style='color: red'>Board ID: " + result.id + "</h3>";
+      div.innerHTML += "Board Name: " + result.name + "<br />";
+    })
+    .catch(error => console.log('error', error));
 }
 
 
+function deleteBoard(boardId) {
+  var requestOptions = {
+    method: 'DELETE',
+    headers: {
+      "Content-Type": "application/json"
+    },
+    redirect: 'follow'
+  };
 
-//////////////////////get board by ID/////////////////////////////
-
-
-function getBoardByID() {
-    var myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-
-    var requestOptions = {
-        method: 'GET',
-        headers: myHeaders,
-        redirect: 'follow'
-    };
-
-    fetch("http://localhost:8080/api/board/2", requestOptions)
-        .then(response => response.text())
-        .then(result => console.log(result))
-        .catch(error => console.log('error', error));
-}
-
-
-
-/////////////////////////delete Board by ID//////////////////////
-
-
-
-function deleteBoardByID() {
-    var myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-
-    var requestOptions = {
-        method: 'DELETE',
-        headers: myHeaders,
-        redirect: 'follow'
-    };
-
-    fetch("http://localhost:8080/delete/1", requestOptions)
-        .then(response => response.text())
-        .then(result => console.log(result))
-        .catch(error => console.log('error', error));
-}
-
-
-
-
-
-
-/////////////////////////////////to create card////////////////////////////////////
-function createCard(){
-var myHeaders = new Headers();
-myHeaders.append("Content-Type", "application/json");
-
-var raw = JSON.stringify({
-  "createdDate": null,
-  "updatedDate": null,
-  "isActive": null,
-  "cardId": 2,
-  "boardModel": null,
-  "title": "",
-  "section": "section",
-  "description": "description"
-});
-
-var requestOptions = {
-  method: 'POST',
-  headers: myHeaders,
-  body: raw,
-  redirect: 'follow'
-};
-
-fetch("http://localhost:8080/createCard", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
+  fetch("/" + boardId, requestOptions)
+    .then(response => {
+      if (response.status === 200) {
+        console.log("Board deleted successfully!");
+      } else {
+        console.log("Failed to delete board.");
+      }
+    })
+    .catch(error => console.log('error', error));
 }
 
 
 
 
-/////////////////////////to get all Cards/////////////////////////////
 
-function getallCards(){
-var myHeaders = new Headers();
-myHeaders.append("Content-Type", "application/json");
-
-var raw = JSON.stringify({
-  "createdDate": null,
-  "updatedDate": null,
-  "isActive": null,
-  "cardId": 2,
-  "boardModel": null,
-  "title": "",
-  "section": "section",
-  "description": "description"
-});
-
-var requestOptions = {
-  method: 'GET',
-  headers: myHeaders,
-  body: raw,
-  redirect: 'follow'
-};
-
-fetch("http://localhost:8080/getCard", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
-}
-
-
-/////////////////////////get card by id////////////////////////
-
-
-function getCardByID(){
-
-var myHeaders = new Headers();
-myHeaders.append("Content-Type", "application/json");
-
-var raw = JSON.stringify({
-  "createdDate": null,
-  "updatedDate": null,
-  "isActive": null,
-  "cardId": 2,
-  "boardModel": null,
-  "title": "",
-  "section": "section",
-  "description": "description"
-});
-
-var requestOptions = {
-  method: 'GET',
-  headers: myHeaders,
-  body: raw,
-  redirect: 'follow'
-};
-
-fetch("http://localhost:8080/api/Card/2", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
-}
-
-
-/////////////////////////Delet card by id //////////////////////
-
-function deletCard(){
-
-    var myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-    
-    var raw = JSON.stringify({
-      "createdDate": null,
-      "updatedDate": null,
-      "isActive": null,
-      "cardId": 2,
-      "boardModel": null,
-      "title": "",
-      "section": "section",
-      "description": "description"
-    });
-    
-    var requestOptions = {
-      method: 'DELETE',
-      headers: myHeaders,
-      body: raw,
-      redirect: 'follow'
-    };
-    
-    fetch("http://localhost:8080/deleteCard/1", requestOptions)
-      .then(response => response.text())
-      .then(result => console.log(result))
-      .catch(error => console.log('error', error));
-
-
-
-}
-
-//////////////////update card ///////////////////////
-
-
-function updateCard(){
-
-
-    var myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-    
-    var raw = JSON.stringify({
-      "createdDate": null,
-      "updatedDate": null,
-      "isActive": null,
-      "cardId": 2,
-      "boardModel": null,
-      "title": "",
-      "section": "section",
-      "description": "description"
-    });
-    
-    var requestOptions = {
-      method: 'PUT',
-      headers: myHeaders,
-      body: raw,
-      redirect: 'follow'
-    };
-    
-    fetch("http://localhost:8080/UpdateCard/2", requestOptions)
-      .then(response => response.text())
-      .then(result => console.log(result))
-      .catch(error => console.log('error', error));
-
-
-
-
-
-
-
-
-
-
-}
